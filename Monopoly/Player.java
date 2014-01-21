dd8801b27f5a26adb06bb3c2ca7b3348e8f5f870
Kemraj Sobhai <kbsobhai@gmail.com>
2014-01-21 08:35:44 -0500

    added Gui

--- Monopoly/Player.java
+++ Monopoly/Player.java
@@ -5,52 +5,53 @@ import java.io.*;
 public class Player{
     protected String token, name;
     protected int money, initRoll, order, lastRoll;
-    protected Property[] props;
+    protected ArrayList<Property> props;
     protected boolean jail, jailCard;
-	//also if they have a get out of jail card?
+    //also if they have a get out of jail card?
     
     public Player (String plName, String plToken){
-		money = 1500;
-		props = new Property[28];
-		jail = false; 
+	money = 1500;
+	props = new ArrayList<Property>();
+	jail = false; 
     }
     
     public void addMoney(int amt) {//adds money for any reason
-		money = money + amt;
+	money = money + amt;
     }
     
     public boolean loseMoney(int amt) {//deducts money for any reason
-		if (money >= amt){
-			money = money - amt;
-			return true; 
-		}	    
-		else
-			return false;
+	if (money >= amt){
+	    money = money - amt;
+	    return true; 
+	}	    
+	else
+	    return false;
     }
-	
+    
     public boolean giveMoney(Player other, int amt) {//gives money from one player to another
-		if (this.money >= amt){
-			this.addMoney(-amt);
-			other.addMoney(amt);
-			return true; 
-		}	    
-		else
-			return false;
+	if (this.money >= amt){
+	    this.addMoney(-amt);
+	    other.addMoney(amt);
+	    return true; 
+	}	    
+	else
+	    return false;
+    }
+    
+    public int getMoney() {
+	return this.money;
+    }
+    
+    public int numOfColourSet(String colour){
+	//returns the number of properties in a set that is held
+	int count = 0;
+	for (int i = 0; i < props.size(); i++) {
+	    if (colour.equals(props.get(i).getColour())) {
+		count++; 
+	    }
+	}
+	return count;
     }
 
-    public int getMoney() {
-		return this.money;
-	}
-	
-	public int numOfColourSet(String colour){
-	//returns the number of properties in a set that is held
-		int count = 0;
-		for (int i = 0; i < props.length; i++) {
-			if (colour.equals(props[i].getColour())) {
-				count++; 
-			}
-		}
-		return count;
-	}
-
+    
 }

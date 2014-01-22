6e4697714bb342ce15a19dc1ee18c1521aac94ce
Kemraj Sobhai <kbsobhai@gmail.com>
2014-01-22 08:43:14 -0500

    update

--- Monopoly/Board.java
+++ Monopoly/Board.java
@@ -5,89 +5,110 @@ public class Board {
     public Tiles [][] board;
     private boolean play;
     protected Player P1, P2, P3, P4;
-
+    protected String chanceC, communityC;
+    protected Cards deck;
+    private int doubleRolls;
+    
     
     public Board() {
-		//sub classes Property and EventSquares in array of Tiles
-		board = new Tiles [4] [9];
-		board [0] [0] = new EventTile("Go");
-		board [0] [1] = new Property("Mediterranean");
-		board [0] [2] = new EventTile("C.C.");
-		board [0] [3] = new Property("Baltic");
-		board [0] [4] = new EventTile("Income Tax");
-		board [0] [5] = new Property("Reading");
-		board [0] [6] = new Property("Oriental");
-		board [0] [7] = new EventTile("Chance");
-		board [0] [8] = new Property("Vermont");
-		board [0] [9] = new Property("Connecticut");
-		//Begin 2nd Side
-		board [1] [0] = new EventTile("Jail");
-		board [1] [1] = new Property("St. Charles");
-		board [1] [2] = new Property("Electric");
-		board [1] [3] = new Property("States");
-		board [1] [4] = new Property("Virginia");
-		board [1] [5] = new Property("Pennsylvania R.R.");
-		board [1] [6] = new Property("St. James");
-		board [1] [7] = new EventTile("C.C.");
-		board [1] [8] = new Property("Tennessee");
-		board [1] [9] = new Property("New York");
-		//Begin 3rd Side
-		board [2] [0] = new EventTile("Free Parking");
-		board [2] [1] = new Property("Kentucky");
-		board [2] [2] = new EventTile("Chance");
-		board [2] [3] = new Property("Indiana");
-		board [2] [4] = new Property("Illinois");
-		board [2] [5] = new Property("B&O");
-		board [2] [6] = new Property("Atlantic");
-		board [2] [7] = new Property("Ventnor");
-		board [2] [8] = new Property("Water");
-		board [2] [9] = new Property("Marvin");
-		//Begin Last Side
-		board [3] [0] = new EventTile("Go To Jail");
-		board [3] [1] = new Property("Pacific");
-		board [3] [2] = new Property("North Carolina");
-		board [3] [3] = new EventTile("C.C.");
-		board [3] [4] = new Property("Pennsylvania Ave");
-		board [3] [5] = new Property("Short Line");
-		board [3] [6] = new EventTile("Chance");
-		board [3] [7] = new Property("Park Place");
-		board [3] [8] = new EventTile("Luxury Tax");
-		board [3] [9] = new Property("Boardwalk");
-
+	//sub classes Property and EventSquares in array of Tiles
+	board = new Tiles [4] [9];
+	board [0] [0] = new EventTile("Go");
+	board [0] [1] = new Property("Mediterranean");
+	board [0] [2] = new EventTile("C.C.");
+	board [0] [3] = new Property("Baltic");
+	board [0] [4] = new EventTile("Income Tax");
+	board [0] [5] = new Property("Reading");
+	board [0] [6] = new Property("Oriental");
+	board [0] [7] = new EventTile("Chance");
+	board [0] [8] = new Property("Vermont");
+	board [0] [9] = new Property("Connecticut");
+	//Begin 2nd Side
+	board [1] [0] = new EventTile("Jail");
+	board [1] [1] = new Property("St. Charles");
+	board [1] [2] = new Property("Electric");
+	board [1] [3] = new Property("States");
+	board [1] [4] = new Property("Virginia");
+	board [1] [5] = new Property("Pennsylvania R.R.");
+	board [1] [6] = new Property("St. James");
+	board [1] [7] = new EventTile("C.C.");
+	board [1] [8] = new Property("Tennessee");
+	board [1] [9] = new Property("New York");
+	//Begin 3rd Side
+	board [2] [0] = new EventTile("Free Parking");
+	board [2] [1] = new Property("Kentucky");
+	board [2] [2] = new EventTile("Chance");
+	board [2] [3] = new Property("Indiana");
+	board [2] [4] = new Property("Illinois");
+	board [2] [5] = new Property("B&O");
+	board [2] [6] = new Property("Atlantic");
+	board [2] [7] = new Property("Ventnor");
+	board [2] [8] = new Property("Water");
+	board [2] [9] = new Property("Marvin");
+	//Begin Last Side
+	board [3] [0] = new EventTile("Go To Jail");
+	board [3] [1] = new Property("Pacific");
+	board [3] [2] = new Property("North Carolina");
+	board [3] [3] = new EventTile("C.C.");
+	board [3] [4] = new Property("Pennsylvania Ave");
+	board [3] [5] = new Property("Short Line");
+	board [3] [6] = new EventTile("Chance");
+	board [3] [7] = new Property("Park Place");
+	board [3] [8] = new EventTile("Luxury Tax");
+	board [3] [9] = new Property("Boardwalk");
+	
  	//creates cards
-	String [] chancec = new String [16];
-	Cards c = new Cards ();
+	chanceC = new String [16];
+	deck = new Cards ();
 	
 	for (int i = 0; i < 16; i ++) {
-		chancec [i] = c.makechancedeck (i);
+	    chanceC [i] = deck.makechancedeck (i);
 	}
-
+	
 	String [] communityc = new String [16];
-
+	
 	for (int i = 0; i < 16; i ++) {
-		communityc [i] = c.makecommunitydeck (i);
+	    communityC [i] = deck.makecommunitydeck (i);
 	}
 	
-		
+	
     }
-	    
-
-    Random R = new Random ();
-    public void rollDice () {
-		int die1 = R.nextInt (6) + 1;
-		int die2 = R.nextInt (6) + 1;
-		//need location variable for player
-		//a.changelocation();
+    
+    
+    
+    public int rollDie () {
+	Random R = new Random ();
+	return R.nextInt (6) + 1;
+	//int die2 = R.nextInt (6) + 1;
+	//need location variable for player
+	//a.changelocation();
     }
     
     public void playGame (){
-		while (play) {
-		//	P1.move;
-		//	P2.move;
-		//	P3.move;
-		//	P4.move;
-		}
-		System.out.println ("Game Over");
+	while (play) {
+	    //	P1.move;
+	    //	P2.move;
+	    //	P3.move;
+	    //	P4.move;
+	}
+	System.out.println ("Game Over");
     }
+
+    public void playerTurn (Player thing){
+	int die1 = rollDie();
+	int die2 = rollDie();
+	int roll = die1 + die2;
 	
+	if (die1 == die2){
+	    boolean Double = true;
+	    doubleRolls ++;
+	}
+	
+	//this.move(thing, roll)
+
+	/*
+	  if (Double == true && finiish thiss)
+	 */
+    }
+    
 }
--- Monopoly/Cards.java
+++ Monopoly/Cards.java
@@ -1,112 +1,109 @@
 import java.util.*;
 import java.io.*;
 
-public class Cards extends EventTile{
+public class Cards{
     Random r = new Random ();
     public String makecommunitydeck (int a) {
 	if (a == 0){
-		return "Advance to Go- Collect $200";
-	    }
-	if (a == 1){
-		return "Bank error in your favor – Collect $75 ";
-	    }
-	if (a == 2){
-		return "Doctor's fees – Pay $50";
-	    }
-	if (a == 3){
-		return "Get out of jail free – This card may be kept until needed, or sold ";
-	    }
-	if (a == 4){
-		return "Go to jail – go directly to jail – Do not pass Go, do not collect $200";
-	    }
-	if (a == 5){
-		return "It is your birthday- Collect $10 from each player";
-	    }
-	if (a == 6){
-		return "Grand Opera Night – Collect $50 from every player";
-	    }
-	if (a == 7){
-		return "Income Tax refund – Receive $20 ";
-	    }
-	if (a == 8){
-		return "Pay Hospital Fees- Pay $100";
-	    }
-	if (a == 9){
-		return "Pay School Fees- Pay $50";
-	    }
-	if (a == 10){
-		return "Receive Consultancy Fee- Receive $25";
-	    }
-
-	if (a == 11){
-		return "You are assessed for street repairs – Pay $40 per house, $115 per hotel ";
-	    }
-	if (a == 12){
-		return "You have won second prize in a beauty contest– Collect $10 ";
-	    }
-	if (a == 13){
-		return "You inherit money - Receive $100";
-	    }
-	if (a == 14){
-		return "Sold Stocks- Collect $50 ";
-	    }
-	if (a == 15){
-		return "Holiday Fund matures - Collect $100 )";
-	    }
+	    return "Advance to Go- Collect $200";
 	}
-
+	if (a == 1){
+	    return "Bank error in your favor – Collect $75 ";
+	}
+	if (a == 2){
+	    return "Doctor's fees – Pay $50";
+	}
+	if (a == 3){
+	    return "Get out of jail free – This card may be kept until needed, or sold ";
+	}
+	if (a == 4){
+	    return "Go to jail – go directly to jail – Do not pass Go, do not collect $200";
+	}
+	if (a == 5){
+	    return "It is your birthday- Collect $10 from each player";
+	}
+	if (a == 6){
+	    return "Grand Opera Night – Collect $50 from every player";
+	}
+	if (a == 7){
+	    return "Income Tax refund – Receive $20 ";
+	}
+	if (a == 8){
+	    return "Pay Hospital Fees- Pay $100";
+	}
+	if (a == 9){
+	    return "Pay School Fees- Pay $50";
+	}
+	if (a == 10){
+	    return "Receive Consultancy Fee- Receive $25";
+	}
+	
+	if (a == 11){
+	    return "You are assessed for street repairs – Pay $40 per house, $115 per hotel ";
+	}
+	if (a == 12){
+	    return "You have won second prize in a beauty contest– Collect $10 ";
+	}
+	if (a == 13){
+	    return "You inherit money - Receive $100";
+	}
+	if (a == 14){
+	    return "Sold Stocks- Collect $50 ";
+	}
+	if (a == 15){
+	    return "Holiday Fund matures - Collect $100 )";
+	}
+    }
+    
     public String makechancedeck (int a) {
 	if (a == 0){
-		return "Advance to Go- Collect $200";
-	    }
+	    return "Advance to Go- Collect $200";
+	}
 	if (a == 1){
-		return "Advance to Illinois Ave ";
-	    }
+	    return "Advance to Illinois Ave ";
+	}
 	if (a == 2){
-		return "Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown. ";
-	    }
+	    return "Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown. ";
+	}
 	if (a == 3){
-		return "Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.";
-	    }
+	    return "Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.";
+	}
 	if (a ==4) {
-		return "You have won a crossword competition- Collect $150";
-	    }
+	    return "You have won a crossword competition- Collect $150";
+	}
 	if (a == 5){
-		return "Advance to St. Charles Place – If you pass Go, collect $200 ";
-	    }
+	    return "Advance to St. Charles Place – If you pass Go, collect $200 ";
+	}
 	if (a == 6){
-		return "Bank pays you dividend of $50- Collect $50";
-	    }
+	    return "Bank pays you dividend of $50- Collect $50";
+	}
 	if (a == 7){
-		return "Get out of Jail free – this card may be kept until needed, or traded/sold";
-	    }
+	    return "Get out of Jail free – this card may be kept until needed, or traded/sold";
+	}
 	if (a == 8){
-		return "Go back 3 spaces ";
-	    }
+	    return "Go back 3 spaces ";
+	}
 	if (a == 9){
-		return "Go directly to Jail – do not pass Go, do not collect $200 ";
-	    }
+	    return "Go directly to Jail – do not pass Go, do not collect $200 ";
+	}
 	if (a == 10){
-		return "Make general repairs on all your property – for each house pay $25 – for each hotel $100 ";
-	    }
-
+	    return "Make general repairs on all your property – for each house pay $25 – for each hotel $100 ";
+	}
+	
 	if (a == 11){
-		return "Pay poor tax- Pay $15";
-	    }
+	    return "Pay poor tax- Pay $15";
+	}
 	if (a == 12){
-		return "Take a trip to Reading Railroad – if you pass Go collect $200";
-	    }
+	    return "Take a trip to Reading Railroad – if you pass Go collect $200";
+	}
 	if (a == 13){
-		return "Take a walk on the Boardwalk – advance to Boardwalk ";
-	    }
+	    return "Take a walk on the Boardwalk – advance to Boardwalk ";
+	}
 	if (a == 14){
-		return "You have been elected chairman of the board – Pay each player $50 ";
-	    }
+	    return "You have been elected chairman of the board – Pay each player $50 ";
+	}
 	if (a == 15){
-		return "Your building loan matures – Collect $150";
-	    }
+	    return "Your building loan matures – Collect $150";
 	}
     }
 }
-
-        

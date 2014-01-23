00b458d0afaa4ce26106c25fcdd70b7d93bc9c5d
Ishmam Choudhury <i.choudhury9@gmail.com>
2014-01-23 02:47:23 -0500

    Gui. Can't make heads or tails of it UGH

--- Monopoly/Gui.java
+++ Monopoly/Gui.java
@@ -1,57 +1,35 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class Gui extends JFrame implements ActionListener{
-	JPanel mypanel;
-	JButton mybutton;
-	JLabel mylabel;
+public class Gui extends JFrame /* implements ActionListener */{
 	
 	public Gui(){
-		//this is the frame overall
-		mypanel = new JPanel();
-		
-		/* this is a clickable button, 
-		but needs an action listener to do something about it */
-		mybutton = new JButton("OK");
-		mybutton.addActionListener(this);
-		
-		//this puts a label next to the button
-		mylabel = new JLabel("show something");
-		
-		//adds the parts to the overall button
-		mypanel.add(mybutton);
-		mypanel.add(mylabel);
-		this.add(mypanel);
-	}
-
-	public static void main(String[] args) {
-		//instantiates the Gui
-		Gui first = new Gui();
-		//sets the name on the Title Bar - This is Monopoly
-		first.setTitle("Monopoly");
-		//sets size in pixels, length x width
-		first.setSize(700,700);
-		/* 
-		Sets the operation that will happen by default when the user initiates a "close" on this frame. You must specify one of the following choices:
-		DO_NOTHING_ON_CLOSE (defined in WindowConstants): Don't do anything; require the program to handle the operation in the windowClosing method of a registered WindowListener object.
-		HIDE_ON_CLOSE (defined in WindowConstants): Automatically hide the frame after invoking any registered WindowListener objects.
-		DISPOSE_ON_CLOSE (defined in WindowConstants): Automatically hide and dispose the frame after invoking any registered WindowListener objects.
-		EXIT_ON_CLOSE (defined in JFrame): Exit the application using the System exit method. Use this only in applications
-		*/
-		first.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		/* sets whether the frame is visible when its parent is visible.
-		Components are initially visible, except top level Components
-		such as Frame Objects. (From Oracle docs) */
-		first.setVisible(true);
-	} 
+	JPanel pane = new JPanel();
+	pane.setLayout(new GridBagLayout());
+	GridBagConstraints c = new GridBagConstraints();
+	c.fill = GridBagConstraints.HORIZONTAL;
 	
-	public void actionPerformed(ActionEvent event) {
-		if (event.getSource() == mybutton) { //if where the event originated is the button
-			//mylabel.setText("My button clicked"); //puts a label next to the button
-			System.exit(0);
-		}
+	
+	//For each component to be added to this container:
+	//...Create the component...
+	//...Set instance variables in the GridBagConstraints instance...
+	JButton roll;
+	roll = new JButton("Roll");
+	c.gridx = 0;
+	c.gridy = 0; 
+	c.weightx = 0.5;
+	//c.weighty = 0.5;
+	pane.add(roll, c);
+	}
+	
+	public static void main(String[] args) {
+		Gui one = new Gui();
+		one.setTitle("Monopoly");
+		one.pack(); //auto-sizes instead of setsize(width, height);
+		one.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		one.setVisible(true);
 	}
 }
 
+http://docs.oracle.com/javase/tutorial/uiswing/layout/gridbag.html
---
+++ Monopoly/GuiTest.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GuiTest extends JFrame implements ActionListener{
+	JPanel mypanel;
+	JButton mybutton;
+	JLabel mylabel;
+	
+	public GuiTest(){
+		//this is the frame overall
+		mypanel = new JPanel();
+		
+		/* this is a clickable button, 
+		but needs an action listener to do something about it */
+		mybutton = new JButton("OK");
+		mybutton.addActionListener(this);
+		
+		//this puts a label next to the button
+		mylabel = new JLabel("show something");
+		
+		//adds the parts to the overall button
+		mypanel.add(mybutton);
+		mypanel.add(mylabel);
+		this.add(mypanel);
+	}
+
+	public static void main(String[] args) {
+		//instantiates the Gui
+		GuiTest first = new GuiTest();
+		//sets the name on the Title Bar - This is Monopoly
+		first.setTitle("Monopoly");
+		//sets size in pixels, length x width
+		first.setSize(700,700);
+		/* 
+		Sets the operation that will happen by default when the user initiates a "close" on this frame. You must specify one of the following choices:
+		DO_NOTHING_ON_CLOSE (defined in WindowConstants): Don't do anything; require the program to handle the operation in the windowClosing method of a registered WindowListener object.
+		HIDE_ON_CLOSE (defined in WindowConstants): Automatically hide the frame after invoking any registered WindowListener objects.
+		DISPOSE_ON_CLOSE (defined in WindowConstants): Automatically hide and dispose the frame after invoking any registered WindowListener objects.
+		EXIT_ON_CLOSE (defined in JFrame): Exit the application using the System exit method. Use this only in applications
+		*/
+		first.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/* sets whether the frame is visible when its parent is visible.
+		Components are initially visible, except top level Components
+		such as Frame Objects. (From Oracle docs) */
+		first.setVisible(true);
+	} 
+	
+	public void actionPerformed(ActionEvent event) {
+		if (event.getSource() == mybutton) { //if where the event originated is the button
+			mylabel.setText("My button clicked"); //puts a label next to the button
+			//System.exit(0);
+		}
+	}
+}
+
